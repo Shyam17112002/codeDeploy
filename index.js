bb141a1a4ac31412fe10d3448ce82caf6d11fac3
Ayush17112002 <aayushgupta023@gmail.com>
2023-01-11 14:27:06 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 app.use('/', (req,res)=>{
-    return res.send("Active");
+    return res.send("Active me");
 })
 app.listen(3000,()=>{
     console.log('listening');
